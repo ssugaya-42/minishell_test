2882cb838867358ffa1e821b66b6ec99280a858c
ssugaya <soshin.suga@gmail.com>
2026-03-24 23:05:16 +0900

    refactor: env

---
+++ src/env/env_to_envp.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_to_envp.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssugaya <ssugaya@student.42.fr>            #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-03-24 13:32:59 by ssugaya           #+#    #+#             */
+/*   Updated: 2026-03-24 13:32:59 by ssugaya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shell.h"
+
+static int	env_count(t_env *env)
+{
+	int	count;
+
+	count = 0;
+	while (env)
+	{
+		count++;
+		env = env->next;
+	}
+	return (count);
+}
+
+static char	*join_env_entry(char *key, char *value)
+{
+	char	*tmp;
+	char	*entry;
+
+	if (!value)
+		return (ft_strdup(key));
+	tmp = ft_strjoin(key, "=");
+	if (!tmp)
+		return (NULL);
+	entry = ft_strjoin(tmp, value);
+	free(tmp);
+	return (entry);
+}
+
+char	**env_to_envp(t_env *env)
+{
+	char	**envp;
+	int		i;
+
+	envp = malloc(sizeof(char *) * (env_count(env) + 1));
+	if (!envp)
+		return (NULL);
+	i = 0;
+	while (env)
+	{
+		envp[i] = join_env_entry(env->key, env->value);
+		if (!envp[i])
+		{
+			free_str_array(envp);
+			return (NULL);
+		}
+		i++;
+		env = env->next;
+	}
+	envp[i] = NULL;
+	return (envp);
+}
---
+++ src/env/env_unset.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_unset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssugaya <ssugaya@student.42.fr>            #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-03-24 13:32:18 by ssugaya           #+#    #+#             */
+/*   Updated: 2026-03-24 13:32:18 by ssugaya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shell.h"
+
+int	env_unset_value(t_env **env, char *key)
+{
+	t_env	*cur;
+	t_env	*prev;
+
+	if (!env || !*env || !key)
+		return (0);
+	cur = *env;
+	prev = NULL;
+	while (cur)
+	{
+		if (ft_strcmp(cur->key, key) == 0)
+		{
+			if (prev)
+				prev->next = cur->next;
+			else
+				*env = cur->next;
+			free(cur->key);
+			free(cur->value);
+			free(cur);
+			return (1);
+		}
+		prev = cur;
+		cur = cur->next;
+	}
+	return (1);
+}
